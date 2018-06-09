186d939d1a6d0bf5c1ded9e0e1eeb82f3e9c7b20
MykolaGalian <gnnkolya@gmail.com>
2018-06-09 14:18:29 +0300

    Добавьте файлы проекта.

---
+++ lab11/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace lab11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ve = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // ve
+            // 
+            this.ve.Enabled = true;
+            this.ve.Interval = 1;
+            this.ve.Tick += new System.EventHandler(this.ve_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(512, 526);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Перемещение изображения по произвольной траектории";
+            this.Load += new System.EventHandler(this.Load_O);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer ve;
+    }
+}
+
---
+++ lab11/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace lab11
+{
+    public partial class Form1 : Form
+    {
+        private int _x; //начальные координаты подвижного обьекта
+        private int _y;
+
+        private int _dx; //шаг
+        private int _dy;
+        private int b_size = 45; // габаритный размер картинки "смайлик" в пикселях 
+        private int r_size = 186; // габариты неподвижного обьекта
+        private int r_left_up_point_x = 160;  // координаты неподвижного обьекта
+        private int r_left_up_point_y = 180;
+                     
+        public Form1()
+        {
+            InitializeComponent();
+            _x = 50; 
+            _y = 150;
+            _dx = 2; 
+            _dy = 2;
+        }
+         
+        private void ve_Tick(object sender, EventArgs e)
+        {
+            MoveSmile();     // ф-ия управления движения обьектом (отрисовка по таймеру, каждую 1 мс)      
+        }
+                           
+        private void Load_O(object sender, EventArgs e)
+        {
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);  // буферизация для снижения мерцания изображения
+        }
+        
+        private void Form1_Paint(object sender, PaintEventArgs e) //получаем ссылку на графический объект "e" как часть PaintEventArgs в событии Paint формы
+        {
+            Image smile_image = Image.FromFile("smile.jpg"); // создание картинки
+            Image wall_image = Image.FromFile("wall7.jpg");
+
+            e.Graphics.DrawImage(smile_image, new Rectangle(_x, _y, b_size, b_size)); //Рисует заданный объект Image в заданном месте, используя указанный размер. 
+                                                                                      // создание прямоугольника для подвижного обьекта (_x, _y меняются)
+
+            e.Graphics.DrawImage(wall_image, r_left_up_point_x-5, r_left_up_point_y-5);       // отрисовка картинки неподвижного обьекта
+        }
+
+        private void MoveSmile() {
+
+            Rectangle wall_XL = new Rectangle(r_left_up_point_x - 7, r_left_up_point_y, 1, r_size);          // создание прямоугольника для неподвижного обьекта(лево) 
+            Rectangle wall_XR = new Rectangle(r_left_up_point_x + r_size + 7, r_left_up_point_y, 1, r_size);    // создание прямоугольника для неподвижного обьекта(право) 
+            Rectangle wall_YU = new Rectangle(r_left_up_point_x, r_left_up_point_y - 7, r_size, 1);          // создание прямоугольника для неподвижного обьекта(верх) 
+            Rectangle wall_YB = new Rectangle(r_left_up_point_x, r_left_up_point_y + r_size + 7, r_size, 1);    // создание прямоугольника для неподвижного обьекта(низ) 
+            
+            Rectangle ball_image_ = new Rectangle(_x, _y, b_size, b_size);  // создание (копии) прямоугольника (не отрисовываем) для подвижного обьекта, для проверки пересечения
+
+            if ( (_x < 0) || (_x > this.ClientSize.Width-45)  )  _dx = -_dx;                                 // отскок от левого и правого края формы
+            else if (wall_XL.IntersectsWith(ball_image_) || wall_XR.IntersectsWith(ball_image_)) _dx = -_dx;  // отскок от левого и правого края неподвижного обьекта
+
+            else if ((_y < 0) || (_y > this.ClientSize.Height-45) )  _dy = -_dy;                             //отскок от верхнего и нижнего края формы
+            else if (wall_YU.IntersectsWith(ball_image_) || wall_YB.IntersectsWith(ball_image_)  ) _dy = -_dy; //отскок от верхнего и нижнего края неподвижного обьекта
+
+            _x += _dx;  // приращение движения обьекта
+            _y += _dy;
+            
+            Invalidate(); // стирание для последующей перерисовки
+        }
+    }
+}
